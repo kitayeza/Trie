75ddc0f8161daf4c6b12de97fff9c4908d93dbf3
Yana Zhen <onetwosevenooone23@gmail.com>
2022-08-23 17:25:01 +0300

    Add test

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-#include "../Trie.h"
+#include "Trie.h"
 #include "gtest/gtest.h"
+
+namespace testing
+{
+    static const std::vector<std::string> vec = {
+            "condemnation","gutter","enzyme","cocktail","respondent","tracker","exchange","contingent",
+            "intermission","darling","interrogator","landing","chart","stockpile","commander","program",
+            "fillet","dud","mink","snowstorm","sandwich","sole","cleanser","tubing","pineapple","discrimination",
+            "traffic","tic","bulimia","presence","place","setup","streetlight","drift","championship","atbat",
+            "wrench","asterisk","profiling","meatball","aviator","storm","doubling","pastime","mysticism","crowd",
+            "pc","whiff","throwback","version","vacancy","mustard","daughter","diving","conspiracy","upside","designer",
+            "doctor","transfusion","restitution","stool","angler","preface","quality","bowler","afternoon","sweat","end",
+            "trek","lilac","emphasis","jockey","brain","fence","looter","binoculars","nutrient","juniper","merchant",
+            "federalism","stretcher","ethics","periodical","knot","repellent","critter","model","biosolids","section",
+            "colonizer","newsman","quiver","liquidity","urgency","hormone","graveyard","filipino","applause","forest","dietitian"
+    };
+}
 
 TEST(testcase,insert)
 {
@@ -22,22 +38,9 @@ TEST(testcase, find)
 {
     nm::Trie<int> trie;
 
-    std::vector<std::string> vec = {
-            "condemnation","gutter","enzyme","cocktail","respondent","tracker","exchange","contingent",
-            "intermission","darling","interrogator","landing","chart","stockpile","commander","program",
-            "fillet","dud","mink","snowstorm","sandwich","sole","cleanser","tubing","pineapple","discrimination",
-            "traffic","tic","bulimia","presence","place","setup","streetlight","drift","championship","atbat",
-            "wrench","asterisk","profiling","meatball","aviator","storm","doubling","pastime","mysticism","crowd",
-            "pc","whiff","throwback","version","vacancy","mustard","daughter","diving","conspiracy","upside","designer",
-            "doctor","transfusion","restitution","stool","angler","preface","quality","bowler","afternoon","sweat","end",
-            "trek","lilac","emphasis","jockey","brain","fence","looter","binoculars","nutrient","juniper","merchant",
-            "federalism","stretcher","ethics","periodical","knot","repellent","critter","model","biosolids","section",
-            "colonizer","newsman","quiver","liquidity","urgency","hormone","graveyard","filipino","applause","forest","dietitian"
-    };
-
     int i = 0;
 
-    for (const auto& str : vec)
+    for (const auto& str : testing::vec)
     {
         i++;
         trie.insert(str, i);
@@ -45,11 +48,34 @@ TEST(testcase, find)
 
     i = 0;
 
-    for (const auto& str : vec)
+    for (const auto& str : testing::vec)
     {
         i++;
         EXPECT_EQ(i, trie.search(str));
     }
+}
+
+TEST(testcase,starts_with)
+{
+    nm::Trie<int> trie;
+
+    int i = 0;
+
+    char val = 'c';
+
+    for (const auto& str : testing::vec)
+    {
+        if(str[0] == val)
+        {
+            i++;
+            trie.insert(str, i);
+        }
+    }
+
+    std::vector<std::string> v;
+    trie.startsWith("c",v);
+    ASSERT_TRUE(v.size() == i);
+
 }
 
 int main(){
